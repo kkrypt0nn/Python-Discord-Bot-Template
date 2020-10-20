5a803316d1062f48ddedde7b47af30eefe48e427
Krypton <43011723+kkrypt0nn@users.noreply.github.com>
2020-10-20 20:55:47 +0200

    Small config.py update

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-BOT_PREFIX = ("YOUR_BOT_PREFIX_HERE") # Can be multiple prefixes, like this: ("!", "?")
+# Can be multiple prefixes, like this: ("!", "?")
+BOT_PREFIX = ("YOUR_BOT_PREFIX_HERE")
 TOKEN = "YOUR_TOKEN_HERE"
 APPLICATION_ID = "YOUR_APPLICATION_ID"
 OWNERS = [123456789, 987654321]
 BLACKLIST = []
+ # Default cogs that I have created for the template
 STARTUP_COGS = [
-    "cogs.general", "cogs.help", "cogs.moderation", "cogs.owner", # Default cogs that I have created for the template
+    "cogs.general", "cogs.help", "cogs.moderation", "cogs.owner",
 ]
