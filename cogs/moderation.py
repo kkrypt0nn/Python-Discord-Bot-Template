3ebe105acb170acff04fa01d6a92254f7bd711d3
coendvrs <47634997+coendvrs@users.noreply.github.com>
2022-12-08 15:18:31 +0100

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -276,7 +276,7 @@ class Moderation(commands.Cog, name="moderation"):
             description=f"**{context.author}** cleared **{len(purged_messages)}** messages!",
             color=0x9C84EF
         )
-        await context.channel.send(embed=embed)
+        await context.send(embed=embed)
 
     @commands.hybrid_command(
         name="hackban",
