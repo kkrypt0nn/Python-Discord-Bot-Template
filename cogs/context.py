3fadf240b0a7f49bf40bd937dfbcf4eeaf024a17
De Vuyst Silas <silas.devuyst@ugent.be>
2023-05-31 18:03:35 +0200

    added remove from OOC game

--- cogs/context.py
+++ cogs/context.py
@@ -9,21 +9,23 @@ from helpers import checks, db_manager
 class Context(commands.Cog, name="context"):
     def __init__(self, bot):
         self.bot = bot
-        self.ctx_menu = app_commands.ContextMenu(
-            name='addcontext',
+        self.ctx_menu_add = app_commands.ContextMenu(
+            name='Add to Out of Context',
             callback=self.context_add,
         )
-        self.bot.tree.add_command(self.ctx_menu)
+        self.bot.tree.add_command(self.ctx_menu_add)
+
+        self.ctx_menu_remove = app_commands.ContextMenu(
+            name='Remove from Out of Context',
+            callback=self.context_remove,
+        )
+        self.bot.tree.add_command(self.ctx_menu_remove)
 
     @checks.not_blacklisted()
     async def context_add(self, interaction: discord.Interaction, message:discord.Message):
         """
         Lets you add a message to the OOC game.
 
-        :param context: The hybrid command context.
-        :param message_id: The hybrid command context.
-        :param about: The user about whom the message is.
-        :param submitted_by: The user who submitted the message.
         """
         submitted_id = interaction.user.id
 
@@ -49,6 +51,45 @@ class Context(commands.Cog, name="context"):
         # alles oke
         embed = discord.Embed(
             description=f"**{message.id}** has been successfully added to the game",
+            color=0x39AC39,
+        )
+        embed.set_footer(
+            text=f"There {'is' if total == 1 else 'are'} now {total} {'message' if total == 1 else 'messages'} in the game"
+        )
+        await interaction.response.send_message(embed=embed, delete_after=30)
+
+
+
+    @checks.not_blacklisted()
+    async def context_remove(self, interaction: discord.Interaction, message:discord.Message):
+        """
+        Lets you remove a message to the OOC game.
+
+        """
+        submitted_id = interaction.user.id
+
+        if not await db_manager.is_in_ooc(message.id):
+            embed = discord.Embed(
+                description=f"**{message.id}** is not in the game.",
+                color=0xE02B2B,
+            )
+            await interaction.response.send_message(embed=embed, delete_after=30)
+            return
+        
+        total = await db_manager.remove_message_from_ooc(message.id, submitted_id)
+
+        # error
+        if total == -1:
+            embed = discord.Embed(
+                description=f"Er is iets misgegaan.",
+                color=0xE02B2B,
+            )
+            await interaction.response.send_message(embed=embed, delete_after=30)
+            return
+        
+        # alles oke
+        embed = discord.Embed(
+            description=f"**{message.id}** has been successfully removed from the game",
             color=0x39AC39,
         )
         embed.set_footer(
