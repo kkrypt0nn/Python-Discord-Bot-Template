707088b9b560d0a1c00ff59f1686cbf4c29173f2
Krypton <root@krypton.ninja>
2023-09-20 08:46:55 +0200

    chore: Updated version in the various files

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -3,7 +3,7 @@ Copyright © Krypton 2019-2023 - https://github.com/kkrypt0nn (https://krypton.n
 Description:
 🐍 A simple template to start to code your own and personalized discord bot in Python programming language.
 
-Version: 6.0.0
+Version: 6.1.0
 """
 
 import os
