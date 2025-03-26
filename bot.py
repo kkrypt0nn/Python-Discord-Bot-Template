0d0998790d8dad864ebb6f0c8cdf25b50c88ca70
thefrenchaxo <thefrenchaxo@gmail.com>
2025-03-26 17:13:02 +0100

    Updating the intents variable

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ intents.message_content = True
 intents.presences = True
 """
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 
 """
 Uncomment this if you want to use prefix (normal) commands.
